119f6aa959cb1c6574b18d2dae3998e8529a79f9
Wendy Montano <47668478+wendymonl@users.noreply.github.com>
2021-03-18 17:06:53 -0500

    Update and rename MakeParksIntoSheets.py to Make sheets in Excel Workbook.py

--- Make sheets in Excel Workbook.py
+++ Make sheets in Excel Workbook.py
@@ -6,11 +6,14 @@ Created on Tue Jul 23 12:29:57 2019
 """
 
 from xlwt import Workbook
-opentxtfile=open("LaredoParks.txt")
+opentxtfile=open("LaredoParks.txt") #all real estate parks in Laredo are in this file
 parkslines=opentxtfile.readlines()
-workbookname="Laredo Industrial Parks"
+
+workbookname="Laredo Industrial Parks" #this workbook will house each industrial park in Laredo. 
 workbookname= Workbook()
 numberofparks=len(parkslines)
+
+#each industrial park will be a sheet, and every building in the park will written into that sheet
 for i in range(0,numberofparks-1):
     parknamewithenter=parkslines[i]
     parkname=parknamewithenter.split()[0]
@@ -18,5 +21,5 @@ for i in range(0,numberofparks-1):
     print(parkname+' GeographicIDs.txt')
     nameoffile=parkname+' GeographicIDs.txt'
     print(nameoffile)
-    opengeographicidtxt=open(nameoffile)
-    geographicid_list=opengeographicidtxt.readlines()
+    opengeographicidtxt=open(nameoffile) 
+    geographicid_list=opengeographicidtxt.readlines() #include Geographic IDs of buildings for sorting later
